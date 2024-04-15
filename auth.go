8da3a7e2ee2f152af927132fea8369ca9d5af9b1
sinascience <110533614+sinascience@users.noreply.github.com>
2024-04-15 17:13:41 +0700

    update app metadata

--- auth.go
+++ auth.go
@@ -31,15 +31,21 @@ type UserCredentials struct {
 	Data     interface{}
 }
 
+type UserAppMetadata struct {
+	Provider string 	`json:"provider"`
+	Providers []string	`json:"providers"`
+}
+
 type User struct {
 	ID                 string                    `json:"id"`
 	Aud                string                    `json:"aud"`
 	Role               string                    `json:"role"`
 	Email              string                    `json:"email"`
+	Phone              string                    `json:"phone"`
 	InvitedAt          time.Time                 `json:"invited_at"`
 	ConfirmedAt        time.Time                 `json:"confirmed_at"`
 	ConfirmationSentAt time.Time                 `json:"confirmation_sent_at"`
-	AppMetadata        struct{ provider string } `json:"app_metadata"`
+	AppMetadata        UserAppMetadata 			 `json:"app_metadata"`
 	UserMetadata       map[string]interface{}    `json:"user_metadata"`
 	CreatedAt          time.Time                 `json:"created_at"`
 	UpdatedAt          time.Time                 `json:"updated_at"`
